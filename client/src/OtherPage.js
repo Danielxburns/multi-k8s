af11c781b64dfbedbffb22bfb84338a591d15ff4
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2019-12-28 21:09:22 -0600

    Updated title and OtherPage

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
-      <Link to="/">Go back home</Link>
+      <p>I'm some other page!</p>
+      <p>I'm just here to add another bit of complexity to the project.</p>
+      <Link to="/">Go to home</Link>
     </div>
   );
 };
